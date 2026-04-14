d82a2676ac323d8f12e070531360eb73878e69d4
THOTA DHEERAJ <dheeraj.thota.0531@gmail.com>
2026-04-15 04:06:12 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+fn main(){
+    let mut tasks: Vec<String> = Vec::new();
+
+    loop {
+        println!("Welcome to Smart Cli, enter add/list/exit");
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to Read");
+        let input = input.trim();
+
+        if input == "exit" {
+            println!("GoodBye");
+            break;
+        }
+
+        if input.starts_with("add"){
+            let task = input[4..].to_string();
+            tasks.push(task);
+            println!("Task Added Successfully");
+        }else if input == "list" {
+            println!("Your Tasks");
+            for (i, task ) in tasks.iter().enumerate(){
+                println!("{}: {}", i+1, task);
+            }
+        }else {
+            println!("Unknown  Command");
+        }
+
+    }
+}
