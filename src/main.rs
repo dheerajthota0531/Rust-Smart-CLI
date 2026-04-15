c0ab239d73627ae406c84bd97c55bb77e4b32426
THOTA DHEERAJ <dheeraj.thota.0531@gmail.com>
2026-04-15 21:21:28 +0530

    changed few things

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::io;
 
 fn main(){
+
+
     let mut tasks: Vec<String> = Vec::new();
 
-    loop {
-        println!("Welcome to Smart Cli, enter add/list/exit");
+
+  
+
+    println!("Welcome To RUST SMART CLI PROJECT ");
+
+    loop{ 
+        println!("Please Enter add/list/exit");
 
         let mut input = String::new();
+
         io::stdin()
             .read_line(&mut input)
-            .expect("Failed to Read");
+            .expect("Failed to read");
+
         let input = input.trim();
 
         if input == "exit" {
@@ -17,18 +26,17 @@ fn main(){
             break;
         }
 
-        if input.starts_with("add"){
+        if input.starts_with("add ") {
             let task = input[4..].to_string();
             tasks.push(task);
             println!("Task Added Successfully");
-        }else if input == "list" {
+        }else if  input.starts_with("list "){
             println!("Your Tasks");
-            for (i, task ) in tasks.iter().enumerate(){
+            for (i, task) in tasks.iter().enumerate(){
                 println!("{}: {}", i+1, task);
             }
         }else {
-            println!("Unknown  Command");
+            println!("Unknown Command");
         }
-
     }
 }
